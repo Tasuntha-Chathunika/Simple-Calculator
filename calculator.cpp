4360cd75ebfdd336860f71ada0328ce105f1aa27
Tasuntha Chathunika <tasunthachathunika@gmail.com>
2025-05-27 00:59:16 +0530

    Add files via upload

--- calculator.cpp
+++ calculator.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main(){
-    char oper;       //Variable initialization
+    char oper;        
     double number1, number2; 
 
     cout<<"Enter the first number: ";
-    cin>>number1;       //Getting user input num1
-
+    cin>>number1;        
     cout<<"Enter the operator [+,-,/,*]: ";
-    cin>>oper;         //Getting user input
+    cin>>oper;          
 
     cout<<"Enter the second number: ";
-    cin>>number2;      //Getting user input num2
+    cin>>number2;       
 
-    if(oper=='+'){ //checking condition using if statement
+    if(oper=='+'){  
         cout<<number1<<"+"<<number2<<"="<<number1+number2<<endl;
     }
     else if(oper=='-'){
